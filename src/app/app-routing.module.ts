3228e690d06bc030e1465a8b6f9b6b0c7f660bc3
sdgsystems <sdgsystems@outlook.com>
2019-07-18 12:43:28 -0500

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { UserDetailComponent } from './components/user-detail/user-detail.compon
 const routes: Routes = [
   { path: '', component: TodoComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'detail/:id', component: UserDetailComponent },
   { path: 'users', component: UserComponent}
 ];
 
